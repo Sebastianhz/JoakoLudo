3c3f827d9b4208e09858d06c8c4b75a0320f96d0
Sebastianhz <39137973+Sebastianhz@users.noreply.github.com>
2018-11-29 15:25:29 -0300

    surrender
    gg

--- JoakoLudo/src/java/DAO/LoginDao.java
+++ JoakoLudo/src/java/DAO/LoginDao.java
@@ -5,29 +5,56 @@
  */
 package DAO;
 
-
 import Model.Login;
-import javax.persistence.EntityManager;
 
+import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
  * @author Pyther
  */
+
 @Service
 public class LoginDao {
+
     @PersistenceContext
- private EntityManager em;
-    @Transactional(rollbackFor = {SevicioException.class})
-     //@Transactional
-    public void create(Login dto) throws SevicioException{
-    em.persist(dto);
-    em.merge(dto);
-    em.remove(dto);
-   
-    }
+    private EntityManager em;
+
+    @Transactional(rollbackFor = {ServicioException.class})
+    //
     
+    public void create(Login dto) throws ServicioException {
+
+        em.persist(dto);
+        //em.merge(dto); actualizar
+        //em.remove(dto); borrar
+        //em.find(Login.class, "111"); buscar pk
+
+    }
+
+    public Login readByUsuario(String usuario) {
+        return em.find(Login.class, usuario);
+
+    }
+
+    public Login readByJPQL(String usuario) {
+        String sql = "select a from Login a where a.usuario =:usuario";
+
+        Query q = em.createQuery(sql);
+        q.setParameter("usuario", usuario);
+        try {
+            return (Login) q.getSingleResult();
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
+    //consulta multiple jpql
+    
+   
 }
